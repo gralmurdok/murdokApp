59e7747e0d6898e725a8101485c29ee165b58cef
Luis Tinitana <ltinitana@ioet.com>
2018-03-24 10:36:53 -0500

    added configuration file, added actions to retrieve channels info

--- src/murdokApp.js
+++ src/murdokApp.js
@@ -1,28 +1,46 @@
-var express    = require('express');        // call express
-var app        = express();                 // define our app using express
-var bodyParser = require('body-parser');
+import express from 'express';
+import bodyParser from 'body-parser';
+import channelService from './actions/ChannelService';
+
+const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = 8080;        // set our port
+var port = 8080;
+var router = express.Router();
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+app.post('/', (req, res) => {
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    console.log(req.body);
+
+    if(req.body.text === 'hola') {
+        console.log('juray')
+        return res.send({
+            response_type: 'in_channel', // public to the channel
+            text: '302: Found',
+            attachments:[
+              {
+                image_url: 'https://http.cat/302.jpg'
+              }
+          ]}
+        )
+    }
+
+    if(req.body.text === 'channels') {
+        channelService.getChannelsList()
+            .then(channels => channels.forEach(x => console.log(x.id)));
+    }
+
+    if(req.body.text === 'users') {
+        console.log(req.body['channel_id'])
+        channelService.getChannelUsers(req.body['channel_id'])
+            .then(users => console.log(users))
+    }
+
+    console.log('asdasdsa');
+    return res.send({text: 'malo malo'});   
 });
 
-// more routes for our API will happen here
-
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
-
-// START THE SERVER
-// =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Murdok app bothering you on port ' + port);
